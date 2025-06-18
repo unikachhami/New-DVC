57938a2d63534adac67c28f776acab2bd6311732
Unik <unikbahadur1852@gmail.com>
2025-06-18 13:27:35 +0545

    third versoning of data

--- mycode.py
+++ mycode.py
@@ -8,8 +8,12 @@ dict = {
 
 df = pd.DataFrame(dict)
 
-new_row = {'name':'Trent','city':'Liberpool'}
+new_row = {'name':'Trent','city':'Liverpool'}
 df.loc[len(df.index)] = new_row
+
+new_rows = {'name':'Dean','city':'Spain'}
+df.loc[len(df.index)] = new_rows
+
 data_dir = 'data'
 os.makedirs(data_dir,exist_ok=True)
 file_path = os.path.join(data_dir,'sample_data.csv')
