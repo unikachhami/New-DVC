07a8c92dfd2ea094c27eb0bf7e0814f17c318405
Unik <unikbahadur1852@gmail.com>
2025-06-18 13:21:50 +0545

    second dvc versoning

--- mycode.py
+++ mycode.py
@@ -8,6 +8,8 @@ dict = {
 
 df = pd.DataFrame(dict)
 
+new_row = {'name':'Trent','city':'Liberpool'}
+df.loc[len(df.index)] = new_row
 data_dir = 'data'
 os.makedirs(data_dir,exist_ok=True)
 file_path = os.path.join(data_dir,'sample_data.csv')
